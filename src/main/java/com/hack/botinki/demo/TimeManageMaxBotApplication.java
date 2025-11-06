18a853ac3ceeacb1c2441e554b808128a94de53d
LevchenkovAlexander <allevchenkov23@gmail.com>
2025-11-06 18:21:29 +0300

    Add files via upload

---
+++ src/main/java/com/hack/botinki/demo/TimeManageMaxBotApplication.java
@@ -0,0 +1,13 @@
+package com.hack.botinki.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManageMaxBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManageMaxBotApplication.class, args);
+	}
+
+}
